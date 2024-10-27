3fbb9d85eb5d84d5b039607164cb741bd99f73c5
sheryar kayani <sheryarali2010@gmail.com>
2024-10-27 23:46:37 +0500

    updated team

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -3,7 +3,6 @@ import Timeline from '../components/timeline'
 import Founders from '../components/founder'
 import ContactUs from '../components/ContactUs'
 
-
 const timelineData = [
   {
     year: '2019',
@@ -35,13 +34,11 @@ const timelineData = [
 const About: React.FC = () => {
   return (
     <div className="bg-gray-900 text-white">
-      
-        <h1 className="text-4xl font-bold mb-8 text-center">About Trendtial</h1>
-        <Founders />
-        <Timeline data={timelineData} />
-        <ContactUs />
-        {/* <AboutSection /> */}
-    
+      <h1 className="text-4xl font-bold mb-8 text-center">About Trendtial</h1>
+      <Founders />
+      <Timeline data={timelineData} />
+      <ContactUs />
+      {/* <AboutSection /> */}
     </div>
   );
 };
