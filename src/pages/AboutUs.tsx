405a7efac8438fa6d17efcf1658250ae4c97fc6d
sheryar kayani <sheryarali2010@gmail.com>
2024-10-27 23:36:41 +0500

    updated team

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Timeline from '../components/timeline'
 import Founders from '../components/founder'
 import ContactUs from '../components/ContactUs'
-import AboutSection from '../components/AboutSection'
+
 
 const timelineData = [
   {
